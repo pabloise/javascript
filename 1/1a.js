08a0c073758a434a9ce808ca98e3a3a6dcf409eb
Pablo Bloise <pabloise10@gmail.com>
2020-10-27 09:33:10 -0300

    first commit

--- 1/1a.js
+++
@@ -1,9 +0,0 @@
-/* Crear dos variables numéricas y utilizar el operador suma para
-guardar el valor de la suma de ambos números en una 3er variable */
-
-var edadMia, edadHermano;
-
-edadMia = 26;
-edadHermano = 30;
-
-var ambasEdades = edadMia + edadHermano;
--- 1/1b.js
+++
@@ -1,8 +0,0 @@
-//Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
-
-var nombre, apellido;
-
-nombre = 'Pablo';
-apellido = 'González';
-
-var nombreCompleto = 'El nombre completo es: ' + nombre + ' ' + apellido;
