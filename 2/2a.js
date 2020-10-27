04f28e685ae22903edd6e5f297eb0e4b1e14ac95
Pablo Bloise <pabloise10@gmail.com>
2020-10-27 10:03:26 -0300

    exercise 2a

---
+++ 2/2a.js
@@ -0,0 +1,7 @@
+//Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).
+
+var herramienta;
+
+herramienta = 'Destornillador';
+
+console.log(herramienta.toUpperCase());
