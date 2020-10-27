265b5b3b069ebc42dbe107d50bf08fdabf96b70a
Pablo Bloise <pabloise10@gmail.com>
2020-10-27 13:06:10 -0300

    exercise 3b

---
+++ 3/3b/js/3b.js
@@ -0,0 +1,8 @@
+//Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
+
+var meses, mesesOrdenados;
+
+meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+mesesOrdenados = meses.sort();
+console.log(mesesOrdenados);
