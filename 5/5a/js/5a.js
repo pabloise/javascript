a9f9fb6eeca20ab2dffc56a147eed97e67df091c
Pablo Bloise <pabloise10@gmail.com>
2020-10-27 17:04:41 -0300

    exercise 5a

---
+++ 5/5a/js/5a.js
@@ -0,0 +1,10 @@
+/* Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle
+for de JavaScript para mostrar una alerta utilizando cada una de las palabras.*/
+
+var jugadores;
+
+jugadores = ['Ayala', 'Torrent', 'Laso', 'Novaretti', 'Blanco'];
+
+for (i=0; i<5; i++) {
+    alert(jugadores[i]);
+}
