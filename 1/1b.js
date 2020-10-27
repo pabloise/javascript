03d11a5f47438a1aad07cdaaa5089b889bec515c
Pablo Bloise <pabloise10@gmail.com>
2020-10-26 23:18:31 -0300

    exercise 1b

---
+++ 1/1b.js
@@ -0,0 +1,8 @@
+//Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+var nombre, apellido;
+
+nombre = 'Pablo';
+apellido = 'González';
+
+var nombreCompleto = 'El nombre completo es: ' + nombre + ' ' + apellido;
