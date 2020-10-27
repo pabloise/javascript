4d72769d32aa7ded4edeac82e155015501c4533d
Pablo Bloise <pabloise10@gmail.com>
2020-10-27 14:57:35 -0300

    exercise 3d

---
+++ 3/3d/3d.js
@@ -0,0 +1,8 @@
+// Quitar un elemento del principio y del final del array (utilizar shift y pop).
+
+var meses;
+
+meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+meses.shift();
+meses.pop();
