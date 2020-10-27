c7e8d2f9957a7879f70b09be7d2faaa0c4a51b9f
Pablo Bloise <pabloise10@gmail.com>
2020-10-27 14:53:55 -0300

    exercise 3c

---
+++ 3/3c/3c.js
@@ -0,0 +1,8 @@
+//Agregar un elemento al principio y al final del array (utilizar unshift y push).
+
+var meses;
+
+meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+meses.unshift('Inicio de año');
+meses.push('Fin de año');
