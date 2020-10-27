2976a5aaf620df1d52f21b7d5fa6115fe6a29eac
Pablo Bloise <pabloise10@gmail.com>
2020-10-27 11:08:26 -0300

    exercise 2e

---
+++ 2/2e.js
@@ -0,0 +1,8 @@
+/* Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco.
+Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf) */
+
+var movie, movieEspacio;
+
+movie = 'Fight Club';
+
+movieEspacio = movie.indexOf(' ');
