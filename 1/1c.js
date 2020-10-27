bc5f8ce737d6a1fbf3d3ab30bf2320ce40ff255a
Pablo Bloise <pabloise10@gmail.com>
2020-10-27 09:54:17 -0300

    exercise 1c

---
+++ 1/1c.js
@@ -0,0 +1,11 @@
+/* Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras
+del string) guardando el resultado la suma una 3er variable (utilizar length). */
+
+
+var ropa, muebles;
+
+ropa = 'Remera';
+alimento = 'Pizza';
+
+var cosasEnCasa = ropa.length + alimento.length;
+
