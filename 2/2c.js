bd0f8f24cd54b53f1bce77ef4150fcc6c0c8c2a9
Pablo Bloise <pabloise10@gmail.com>
2020-10-27 10:18:53 -0300

    exercise 2c

---
+++ 2/2c.js
@@ -0,0 +1,10 @@
+/* Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string
+con los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring). */
+
+var lugar, lugarCorto
+
+lugar = 'Campamento';
+
+lugarCorto = (lugar.substring(7));
+
+
