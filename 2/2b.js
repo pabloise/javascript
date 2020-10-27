dce943cc46c685b001af226a75ce2ae275fcff60
Pablo Bloise <pabloise10@gmail.com>
2020-10-27 10:12:55 -0300

    exercise 2b

---
+++ 2/2b.js
@@ -0,0 +1,8 @@
+/* Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string
+con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring). */
+
+var lugar, lugarCorto;
+
+lugar = 'Departamento';
+
+lugarCorto = (lugar.substring (0,5));
