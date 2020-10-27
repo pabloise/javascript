2e43f92eedd09f07670f6d8b6c66865367147fb5
Pablo Bloise <pabloise10@gmail.com>
2020-10-27 09:38:02 -0300

    exercise 1a

---
+++ 1/1a.js
@@ -0,0 +1,10 @@
+/* Crear dos variables numéricas y utilizar el operador suma para
+guardar el valor de la suma de ambos números en una 3er variable*/
+
+var edadMia, edadHermano;
+
+edadMia = 26;
+edadHermano = 30;
+
+var ambasEdades = edadMia + edadHermano;
+
