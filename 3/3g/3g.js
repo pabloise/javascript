7445cd76b17e8ddd4f7b4f308383c88980929100
Pablo Bloise <pabloise10@gmail.com>
2020-10-27 15:18:11 -0300

    exercise 3g

---
+++ 3/3g/3g.js
@@ -0,0 +1,7 @@
+//Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).
+
+var meses, mesesMenos;
+
+meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+mesesMenos = meses.slice(4,11);
