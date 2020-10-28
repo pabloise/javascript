8235b79e6e04dc76aeb05cfafc983961efe34521
Pablo Bloise <pabloise10@gmail.com>
2020-10-28 15:49:47 -0300

    exercise 6a

---
+++ 6/6a/js/6a.js
@@ -0,0 +1,15 @@
+/*Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y
+guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.*/
+
+var numero1 = 10;
+var numero2 = 4;
+
+function suma (numero1,numero2){
+    return (numero1) + (numero2);
+}
+var resultado = numero1 + numero2;
+console.log (resultado);
+
+
+
+
