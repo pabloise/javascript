37094af294727483402028995d2b16acb9b6673a
Pablo Bloise <pabloise10@gmail.com>
2020-10-27 22:23:10 -0300

    exercise 5d

---
+++ 5/5d/js/5d.js
@@ -0,0 +1,10 @@
+/* Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición,
+es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número 0
+hasta al numero 9. Mostrar por la consola del navegador el al array final (utilizar console.log).*/
+
+var numbers = [''];
+
+for(i=0 ; i < 10; i++) {
+numbers = i;
+console.log(numbers);
+}
