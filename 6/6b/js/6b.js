51c1f31db3047cb8d7115b7265a226474298348b
Pablo Bloise <pabloise10@gmail.com>
2020-10-28 23:15:24 -0300

    exercise 6b

--- 6/6b/js/6b.js
+++ 6/6b/js/6b.js
@@ -1,16 +1,18 @@
 /*A la función suma anterior, agregarle una validación para controlar si alguno de los
 parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene
 error y retornar el valor NaN como resultado.*/
+var numero1 = 10;
+var numero2 = 20;
 
-var numero1 = 2;
-var numero2 = 8;
+function suma (numero1,numero2){
+    return numero1 + numero2;
+}
 
-function suma (numero1, numero2){
-};
-
-if (numero1 === Number && numero2 === Nu){
-        console.log('Son números');
-    }
-else{
-        console.log('No son números');
+suma()
+    if (typeof numero1 !== 'number' || typeof numero2 !== 'number'){
+        alert ('Uno de los parámetros no es un número');
+        console.log('NaN');
+        }
+    else {
+        console.log(numero1 + numero2);
     }
