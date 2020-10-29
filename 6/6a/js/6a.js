79f69116cccc388a3a2d613bed3bf242e2897f80
Pablo Bloise <pabloise10@gmail.com>
2020-10-28 22:17:43 -0300

    exercise 6a with final changes and complete

--- 6/6a/js/6a.js
+++ 6/6a/js/6a.js
@@ -1,16 +1,17 @@
 /*Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y
 guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.*/
 
-var numero1 = 2;
-var numero2 = 8;
+var numero1 = 10;
+var numero2 = 20;
 
-function suma (){
-    return console.log(numero1 + numero2);
-}
-suma();
+function suma(){
+    return numero1 + numero2
+};
 
-var resultado = numero1 + numero2;
-console.log(resultado);
+var result = suma(numero1,numero2);
+console.log(result);
+
+
 
 
 
--- 6/6b/js/6b.js
+++ 6/6b/js/6b.js
@@ -2,3 +2,15 @@
 parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene
 error y retornar el valor NaN como resultado.*/
 
+var numero1 = 2;
+var numero2 = 8;
+
+function suma (numero1, numero2){
+};
+
+if (numero1 === Number && numero2 === Nu){
+        console.log('Son números');
+    }
+else{
+        console.log('No son números');
+    }
